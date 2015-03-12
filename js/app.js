3154cda04672c0da8c334c3dfb73504494050b70
Hans Pétur Jónsson <hpjonsson@gmail.com>
2015-03-12 12:03:42 +0000

    Refactoring

--- js/app.js
+++ js/app.js
@@ -137,7 +137,6 @@ Player.prototype.handleInput = function(dir) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
 var allEnemies = [];
 for (var i = 1; i <= ENEMIES; i++ ) {
     allEnemies.push(new Enemy());
@@ -149,7 +148,6 @@ var player = new Player();
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
-        13: 'enter',
         32: 'space',
         37: 'left',
         38: 'up',
